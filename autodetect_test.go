2528761e17dd71e659931144f2e747c8fb017d65
Pablo Fonseca <pablofonseca777@gmail.com>
2026-08-18 16:39:03 +0100

    add config autodetect from git remote and workflow files

---
+++ autodetect_test.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"errors"
+	"testing"
+)
+
+func TestParseGitHubSlug(t *testing.T) {
+	cases := []struct {
+		in      string
+		want    string
+		wantErr bool
+	}{
+		{"git@github.com:upscopeio/lazy-actions.git", "upscopeio/lazy-actions", false},
+		{"git@github.com:upscopeio/lazy-actions", "upscopeio/lazy-actions", false},
+		{"https://github.com/upscopeio/lazy-actions.git", "upscopeio/lazy-actions", false},
+		{"https://github.com/upscopeio/lazy-actions", "upscopeio/lazy-actions", false},
+		{"ssh://git@github.com/upscopeio/lazy-actions.git", "upscopeio/lazy-actions", false},
+		{"https://github.com/upscopeio/lazy-actions/", "upscopeio/lazy-actions", false},
+		{"https://gitlab.com/foo/bar.git", "", true},
+		{"git@example.com:foo/bar.git", "", true},
+		{"https://github.com/onlyowner", "", true},
+		{"", "", true},
+	}
+	for _, tc := range cases {
+		got, err := parseGitHubSlug(tc.in)
+		if tc.wantErr {
+			if err == nil {
+				t.Errorf("%q: expected error, got %q", tc.in, got)
+				continue
+			}
+			if !errors.Is(err, ErrNotAutoDetectable) {
+				t.Errorf("%q: expected ErrNotAutoDetectable, got %v", tc.in, err)
+			}
+			continue
+		}
+		if err != nil {
+			t.Errorf("%q: unexpected error %v", tc.in, err)
+			continue
+		}
+		if got != tc.want {
+			t.Errorf("%q: got %q, want %q", tc.in, got, tc.want)
+		}
+	}
+}
